5d8f941aa8bf49f096a37dbb29426005807881fe
SebastianHerreraPC <sebehepe00@gmail.com>
2024-07-03 14:05:36 -0600

    script inicio

---
+++ script/script.js
@@ -0,0 +1,7 @@
+//selector
+
+document.addEventListener("DOMContentLoaded", function () {
+  const profileName = document.querySelector(".profile__name");
+  const profileJob = document.querySelector(".profile__job");
+  const buttonEditProfile = document.querySelector(".button__type-edit");
+});
